c15de838318b1a05eb6bcfa17872dee6786c0af1
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-20 23:38:40 +0530

    Update 1260. Shift 2D Grid.java

--- Arrays/1260. Shift 2D Grid.java
+++ Arrays/1260. Shift 2D Grid.java
@@ -32,3 +32,43 @@ class Solution {
         return ans;
     }
 }
+
+// 2nd Approach :- Flatten the Grid 
+
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+
+        k %= total;
+
+        int[] arr = new int[total];
+        int idx = 0;
+
+        for (int[] row : grid) {
+            for (int val : row) {
+                arr[idx++] = val;
+            }
+        }
+
+        int[] shifted = new int[total];
+
+        for (int i = 0; i < total; i++) {
+            shifted[(i + k) % total] = arr[i];
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        idx = 0;
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(shifted[idx++]);
+            }
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}
