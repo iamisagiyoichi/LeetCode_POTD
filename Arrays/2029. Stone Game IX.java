2f50a99d080cd03bd68d7d3894e1b1c89593596e
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-08-17 00:02:47 +0530

    Refactor stoneGameIX method for clarity and efficiency

--- Arrays/2029. Stone Game IX.java
+++ Arrays/2029. Stone Game IX.java
@@ -1,19 +1,22 @@
 class Solution {
+
     public boolean stoneGameIX(int[] stones) {
-        int[] cnt = new int[3];
-
-        for (int x : stones) {
-            cnt[x % 3]++;
+        int cnt0 = 0,
+            cnt1 = 0,
+            cnt2 = 0;
+        for (int val : stones) {
+            int type = val % 3;
+            if (type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
         }
-
-        if (cnt[1] == 0 || cnt[2] == 0) {
-            return false;
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
         }
-
-        if (cnt[0] % 2 == 0) {
-            return true;
-        }
-
-        return cnt[1] >= 3 || cnt[2] >= 3;
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
     }
 }
