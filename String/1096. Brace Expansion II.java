d982d9f5fb261836a068f5827e59c65c896f361e
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-09-25 23:56:44 +0530

    Refactor brace expansion logic with DFS approach

--- String/1096. Brace Expansion II.java
+++ String/1096. Brace Expansion II.java
@@ -1,55 +1,31 @@
 import java.util.*;
 
 class Solution {
-    int idx = 0;
+    Set<String> ans = new TreeSet<>();
 
-    Set<String> parse(String s) {
-        Set<String> res = new TreeSet<>();
-        Set<String> cur = new TreeSet<>();
-        cur.add("");
+    void dfs(String s) {
+        int r = s.indexOf('}');
 
-        while (idx < s.length() && s.charAt(idx) != '}') {
-            if (s.charAt(idx) == ',') {
-                idx++;
-
-                Set<String> next = parse(s);
-
-                res.addAll(cur);
-                res.addAll(next);
-
-                cur.clear();
-                cur.add("");
-            } else {
-                Set<String> part = new TreeSet<>();
-
-                if (s.charAt(idx) == '{') {
-                    idx++;
-                    part = parse(s);
-                    idx++;
-                } else {
-                    part.add(String.valueOf(s.charAt(idx)));
-                    idx++;
-                }
-
-                Set<String> next = new TreeSet<>();
-
-                for (String a : cur) {
-                    for (String b : part) {
-                        next.add(a + b);
-                    }
-                }
-
-                cur = next;
-            }
+        if (r == -1) {
+            ans.add(s);
+            return;
         }
 
-        res.addAll(cur);
-        return res;
+        int l = s.lastIndexOf('{', r);
+
+        String left = s.substring(0, l);
+        String right = s.substring(r + 1);
+        String inside = s.substring(l + 1, r);
+
+        String[] parts = inside.split(",");
+
+        for (String part : parts) {
+            dfs(left + part + right);
+        }
     }
 
     public List<String> braceExpansionII(String expression) {
-        idx = 0;
-        Set<String> ans = parse(expression);
+        dfs(expression);
         return new ArrayList<>(ans);
     }
 }
